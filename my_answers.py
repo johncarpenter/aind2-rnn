615d11590e518d1484a3df4d99c7e8d4bbea8fd2
John Carpenter <jcarpent@gmail.com>
2017-07-21 03:50:06 +0000

    intermediate work

--- my_answers.py
+++ my_answers.py
@@ -4,7 +4,7 @@ from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import LSTM
 import keras
-
+import itertools
 
 # TODO: fill out the function below that transforms the input series 
 # and window-size into a set of input/output pairs for use with our RNN model
@@ -12,6 +12,10 @@ def window_transform_series(series, window_size):
     # containers for input/output pairs
     X = []
     y = []
+    for i in range(len(series)-(window_size)):
+        X.append(series[i:i+window_size])
+        y.append(series[i+window_size:i+window_size+1])
+
 
     # reshape each 
     X = np.asarray(X)
@@ -23,7 +27,12 @@ def window_transform_series(series, window_size):
 
 # TODO: build an RNN to perform regression on our time series input/output data
 def build_part1_RNN(window_size):
-    pass
+
+    model = Sequential()
+    model.add(LSTM(5, input_shape=(window_size,1)))
+    model.add(Dense(1, activation='sigmoid'))
+
+    return model
 
 
 ### TODO: return the text input with only ascii lowercase and the punctuation given below included.
